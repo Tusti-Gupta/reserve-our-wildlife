fee4e94d2b9fc57b195f74d5c58b1d48e1ba6327
Tusti-Gupta <86391913+Tusti-Gupta@users.noreply.github.com>
2023-06-09 01:26:28 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB4sBWmYfhUq9nNhjSTJDLA5QwW_0rTLDc",
+    authDomain: "reserve-our-wildlife.firebaseapp.com",
+    databaseURL: "https://reserve-our-wildlife-default-rtdb.firebaseio.com",
+    projectId: "reserve-our-wildlife",
+    storageBucket: "reserve-our-wildlife.appspot.com",
+    messagingSenderId: "394558315813",
+    appId: "1:394558315813:web:24f1fe9bf0a159452b8ecc"
+};
+firebase.initializeApp(firebaseConfig);
